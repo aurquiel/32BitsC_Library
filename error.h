19720ad09d8dc58800505df84cdb357ead08da7e
edgar <egomez@thefactoryhka.com>
2018-08-08 16:45:45 -0400

    Adding doxygen documentation

--- error.h
+++ error.h
@@ -1,20 +1,39 @@
 #ifndef ERROR_H_INCLUDED
 #define ERROR_H_INCLUDED
 
+/** \brief enum to enumerate the errors of print.c.
+ *
+ * \enum errorPrint
+ *
+ */
 typedef enum {
-    errorSizePrint = -101,
-    errorTypePrint = -102
+    errorSizePrint = -101,          /*!< -101 indicating error of array size */
+    errorTypePrint = -102           /*!< -101 indicating error of type of array */
 } errorPrint;
 
+
+
+/** \brief enum to enumerate the errors of interchange.c.
+ *
+ * \enum errorInterchange
+ *
+ */
 typedef enum {
-    errorSizeInterchange = -201,
-    errorTypeInterchange = -202
+    errorSizeInterchange = -201,    /*!< -201 indicating error of array size */
+    errorTypeInterchange = -202     /*!< -201 indicating error of type of array */
 } errorInterchange;
 
+
+
+/** \brief enum to enumerate the errors of allocator.c.
+ *
+ * \enum errorAllocator
+ *
+ */
 typedef enum {
-    errorSizeAllocator = -301,
-    errorTypeAllocator = -302,
-    errorNoAllocate = -303
+    errorSizeAllocator = -301,  /*!< -301 indicating error of array size */
+    errorTypeAllocator = -302,  /*!< -301 indicating error of type of array */
+    errorNoAllocate = -303      /*!< -301 indicating error of allocation */
 } errorAllocator;
 
 #endif // ERROR_H_INCLUDED
